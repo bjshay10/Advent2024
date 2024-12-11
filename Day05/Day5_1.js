dfae3ef7406f44b0ca7c471931ff7907c8dfee17
bjshay10 <bj.shay@d51schools.org>
2024-12-10 21:44:03 -0700

    startign day 5

---
+++ Day05/Day5_1.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+const readline = require('readline')
+
+fs.readFile('input.txt', function(err, data){
+    console.log(`STARTING`)
+    
+    console.log(`ENDING Toal = ${total}`)
+})
